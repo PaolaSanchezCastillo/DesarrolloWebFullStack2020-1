f5c13c5cdf517ec4195f9c647f99cc74eb6d6a19
PaolaSanchez <paolaescastillo@gmail.com>
2020-09-05 15:24:03 -0500

    Parcial Modulo

--- Angular/MiApp/src/app/app.module.ts
+++ Angular/MiApp/src/app/app.module.ts
@@ -2,34 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BeerListComponent } from './beer/beer-list/beer-list.component';
-import { FavBeerComponent } from './beer/fav-beer/fav-beer.component';
-import { BeerSearchComponent } from './beer/beer-search/beer-search.component';
 import { RouterModule } from '@angular/router';
-import { BeerDetailComponent } from './beer/beer-detail/beer-detail.component';
-import { BeerDetailGuard } from './beer/beer-detail/beer-detail.guard';
-
+import {BeerModule} from './beer/beer.module'
+import {FavBeerComponent} from './beer/fav-beer/fav-beer.component'
 @NgModule({
   declarations: [
-    AppComponent,
-    BeerListComponent,
-    BeerSearchComponent,
-    FavBeerComponent,
-    BeerDetailComponent
+    AppComponent, 
+    FavBeerComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    BeerModule,
     FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'home', component: FavBeerComponent },
       { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: 'beers', component: BeerSearchComponent },
-      { path: 'beers/:id', component: BeerDetailComponent, canActivate: [ BeerDetailGuard ] },
       { path: '**', redirectTo: '/home', pathMatch: 'full' },
     ])
   ],
--- Angular/MiApp/src/app/beer/beer.module.ts
+++ Angular/MiApp/src/app/beer/beer.module.ts
@@ -3,22 +3,21 @@ import {CommonModule} from '@angular/common';
 import {BeerDetailComponent} from './beer-detail/beer-detail.component';
 import {BeerListComponent} from './beer-list/beer-list.component';
 import {BeerSearchComponent} from './beer-search/beer-search.component';
-import {FavBeerComponent} from './fav-beer/fav-beer.component';
 import {FormsModule} from '@angular/forms'; 
 import {RouterModule} from '@angular/router'; 
 import {BeerDetailGuard} from './beer-detail/beer-detail.guard';
-
-
 
 @NgModule({
 declarations : [
     BeerListComponent, 
     BeerDetailComponent, 
-    FavBeerComponent,
     BeerSearchComponent
 ], 
 imports: [
     CommonModule, 
-    
-]
-})
+    FormsModule, 
+    RouterModule.forChild([
+        {path : 'beer', component: BeerSearchComponent}, 
+        {path : 'beer/:id', component :BeerDetailComponent, canActivate:[BeerDetailGuard] }
+    ])]
+})export class BeerModule {}
